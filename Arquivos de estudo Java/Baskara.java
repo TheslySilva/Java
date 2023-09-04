e9ca8b3e00db6b4b047581e430a740e8aca797d7
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-04 10:45:49 -0300

    update Arquivos de estudo Java/Baskara.java

---
+++ Arquivos de estudo Java/Baskara.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner(System.in);
+		//variaveis
+		float a,b,c;
+		double delta,x1,x2;
+		//entrada
+		System.out.print("Coeficiente A : ");
+		a= entrada.nextFloat();
+		System.out.print("Coeficiente B : ");
+		b= entrada.nextFloat();
+		System.out.print("Coeficiente C : ");
+		c = entrada.nextFloat();
+		
+		delta= Math.pow(b,2) - 4 * a *c;
+		
+		//condicoes
+		if (a==0 || delta <0){
+			System.out.print("Esta equacao nao possui raizes reais");
+		}else{
+			x1=(-b + Math.sqrt(delta))/(2*a);
+			x2=(-b - Math.sqrt(delta))/(2*a);
+			
+			//Resultado
+			System.out.println("X1 = "+ x1);
+			System.out.println("X2 = "+ x2);
+		}
+	}
+}
