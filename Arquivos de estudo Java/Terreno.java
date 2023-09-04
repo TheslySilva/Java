880990d78b6611457e55c6dcc3b9f2d4e2355c04
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-03 21:18:46 -0300

    update Arquivos de estudo Java/Terreno.java

---
+++ Arquivos de estudo Java/Terreno.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner(System.in);
+		System.out.print("Digite a largura do terreno: ");
+		float largura= entrada.nextFloat();
+		System.out.print("Digite o comprimento do terreno: ");
+		float  comprimento= entrada.nextFloat();
+		System.out.print("Digite o valor do metro quadrado: ");
+		float mQuadrado= entrada.nextFloat();
+		
+		float area= largura * comprimento;
+		float preco= area * mQuadrado;
+		
+		System.out.println("Area do terreno = "+ area);
+		System.out.print("Preco do terreno = "+ preco);
+		
+		
+	}
+}
