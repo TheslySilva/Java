f67383c4ffee15a111811b456a939fae975b3a9c
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-05 10:01:38 -0300

    update Arquivos de estudo Java/troco.java

---
+++ Arquivos de estudo Java/troco.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner(System.in);
+		System.out.print("Digite o valor do Produto : ");
+		int p= entrada.nextInt();
+		System.out.print("Quantidade comprada : ");
+		int v= entrada.nextInt();
+		System.out.print("Dinheiro entregue : ");
+		int d= entrada.nextInt();
+		
+		
+		
+		if (d >= p *v){
+			int troco= d-p * v;
+			System.out.print("O troco é : R$ " + troco);
+		}else{
+			int troco= p*v -d;
+			System.out.println("DINHEIRO INSULFICIENTE!");
+			System.out.print("FALTA R$ "+ troco);
+		}
+		
+	}
+}
