f513f1b601ee7ba9eea7616fb27b9d0f384b18b3
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-05 09:35:15 -0300

    update Arquivos de estudo Java/operadora.java

---
+++ Arquivos de estudo Java/operadora.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner(System.in);
+		System.out.print("Digite a quantidade de minutos : ");
+		int usado= entrada.nextInt();
+		int min= 100;
+		if (usado <=100){
+			
+			System.out.print("Valor a pagar : R$ 50,00");
+		}else{
+		int conta = (usado-min)*2 +50;
+		System.out.println("Valor a pagar : R$ " + conta);
+			}
+	}
+}
