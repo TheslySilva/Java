d5f950a1997581dd12eb4eb7be95eaabec667112
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-06 08:50:31 -0300

    update Arquivos de estudo Java/Tempo_de_jogo.java

---
+++ Arquivos de estudo Java/Tempo_de_jogo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada= new Scanner(System.in);
+		
+		System.out.print("Hora inicial : ");
+		int inicial= entrada.nextInt();
+		System.out.print("Hora final : ");
+		int final1= entrada.nextInt();
+		
+		if(inicial <= final1 ){
+		int resultado = final1-inicial ;
+			if(resultado ==0){
+				System.out.print("O JOGO DUROU "+24+ " HORA(S)");
+			}else{
+				System.out.print("O JOGO DUROU "+resultado+ " HORA(S)");
+			}
+			
+		}else if(inicial> final1){
+			int resultado=( 24-inicial )+final1;
+			System.out.print("O JOGO DUROU "+resultado+ " HORA(S)");
+		}
+	}
+}
