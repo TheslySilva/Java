60c7806d0cc7389e4d1fea45a5f508787624810b
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-07 11:51:07 -0300

    update Arquivos de estudo Java/Media.java

---
+++ Arquivos de estudo Java/Media.java
@@ -0,0 +1,29 @@
+
+import java.util.Scanner;
+
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int a;
+		double b = 0;
+		double c = 0 ;
+		double d = 0;
+		System.out.println("Digite as idades : ");
+		a = entrada.nextInt();
+		if (a >= 0) {
+			while (a >= 0) {
+				
+				if (a > 0) {
+					b = a + b;
+					c++;
+				}
+				a = entrada.nextInt();
+			}
+			d = b ;
+			System.out.printf("Media :  %.2f %n",  (d/ c));
+		} else {
+			System.out.print("IMPOSSIVEL CALCULAR!");
+		}
+	}
+}
