8071c3f9a8bb25d508885958d1c52abac7b58024
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-08 16:59:16 -0300

    update Arquivos de estudo Java/Sequência_Impares.java

---
+++ Arquivos de estudo Java/Sequência_Impares.java
@@ -0,0 +1,17 @@
+
+import java.util.Scanner;
+
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int num;
+		int i;
+		System.out.print("Digite um numero : ");
+		num = entrada.nextInt();
+		
+		for(i = 1; i <=num; i+=2) {
+			System.out.println(i);
+		}
+	}
+}
