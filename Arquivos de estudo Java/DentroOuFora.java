96cd9e72b23f93ab8ecfbaeca4842176187f6d18
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-08 22:16:22 -0300

    update Arquivos de estudo Java/DentroOuFora.java

---
+++ Arquivos de estudo Java/DentroOuFora.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		int a;
+		int b;
+		int i;
+		int dentro;
+		int fora;
+		System.out.print("Quantos numeros voce vai digitar? : ");
+		a = entrada.nextInt();
+		System.out.println("");
+		dentro = 0;
+		fora = 0;
+		for (i = 1; i <= a; i++) {
+			System.out.print("Digite um numero : ");
+			b = entrada.nextInt();
+			if (b > 10 && b < 20) {
+				dentro++;
+			} else {
+				fora++;
+			}
+		}
+		System.out.println("");
+		System.out.println("Resultado :\n ");
+		
+		
+		System.out.println("Dentro : " + dentro);
+		System.out.print("Fora : " + fora);
+	}
+}
