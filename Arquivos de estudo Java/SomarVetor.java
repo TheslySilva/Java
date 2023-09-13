c95c023c6fe563b5faff35435b7ae0b63d9d32ff
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-13 10:06:40 -0300

    update Arquivos de estudo Java/SomarVetor.java

---
+++ Arquivos de estudo Java/SomarVetor.java
@@ -0,0 +1,34 @@
+
+import java.util.Scanner;
+
+public class MeuProjeto {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int  n,i;
+		double soma,media;
+		//array
+		double[]vetor= new double[5];
+		//
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n= entrada.nextInt();
+		System.out.println("");
+		for (i=0;i<n;i++){
+			System.out.print("Digite um numero : ");
+			vetor[i]= entrada.nextInt();
+		}
+			
+		soma=0;
+		System.out.println("");
+		System.out.print("VALORES = ");
+		for (i=0;i<n;i++){
+			System.out.print(vetor[i]+" ");
+			soma = vetor[i]+soma;
+		}
+		System.out.println("");
+		media= soma/n;
+		System.out.println("SOMA = " + soma);
+		System.out.println("MEDIA = "+media);
+	}
+}
