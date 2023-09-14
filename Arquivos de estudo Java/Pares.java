b806603d169ca2be7998b6140983b8e85f975b26
Thesly Silva <97068069+TheslySilva@users.noreply.github.com>
2023-09-14 07:20:54 -0300

    update Arquivos de estudo Java/Pares.java

---
+++ Arquivos de estudo Java/Pares.java
@@ -0,0 +1,32 @@
+
+import java.util.Scanner;
+
+public class Pares {
+	
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		int n,quant,i;
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		n= entrada.nextInt();
+		System.out.println("");
+		int[] numeros= new int[n];
+		
+		for (i=0;i<n;i++){
+			System.out.print("Digite um numero : ");
+			numeros[i]=entrada.nextInt();
+			System.out.println("");
+		}
+		System.out.print("NUMEROS PARES :");
+		quant= 0;
+		for(i=0;i<n;i++){
+			if (numeros[i]%2 ==0){
+			System.out.print(numeros[i]+" ");
+				quant++;	
+			}
+		}
+		System.out.println("");
+		System.out.println("QUANTIDADE DE PARES = "+quant);
+	}
+}
